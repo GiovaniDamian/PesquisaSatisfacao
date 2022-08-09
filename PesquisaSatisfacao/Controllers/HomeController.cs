207e33cd12f6f7d53dcf33bf38e9965df26920a4
GiovaniDamian <gedamian@gmail.com>
2022-08-09 14:59:03 -0300

    All project

---
+++ PesquisaSatisfacao/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PesquisaSatisfacao.Models;
+using System.Diagnostics;
+
+namespace PesquisaSatisfacao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ActionResult PesquisaSatisfacao()
+        {
+            ViewData["usuario"] = this.Usuario.Name;
+            ViewData["data"] = DateTime.Now;
+
+            var duplicacao = PesquisaModel.EncontrarPesquisaDuplicada(this.Usuario.Name);
+            var datamodal = PesquisaModel.PesquisarDataModal(this.Usuario.Name);
+
+            if (!duplicacao)
+            {
+                var dataPesquisa = Convert.ToDateTime(datamodal);
+                if (DateTime.Now.DayOfYear >= dataPesquisa.DayOfYear && DateTime.Now.TimeOfDay >= dataPesquisa.TimeOfDay)
+                {
+                    PesquisaModel.AtualizarDataModal(this.Usuario.Name, DateTime.Now.AddDays(1));
+                    return PartialView("_PV_PesquisaSatisfacao", ViewData);
+
+                }
+            }
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public bool IncluirPesquisaNoBanco(PesquisaModel form)
+        {
+            var sucesso = PesquisaModel.Incluir(form.Texto, form.Nota, form.Usuario, form.DataRealizada);
+
+            return sucesso;
+        }
+
+
+        public ActionResult IncluirHorarioModal()
+        {
+            var usuario = this.Usuario.Name;
+            var dataModal = DateTime.Now;
+            var horariomodal = PesquisaModel.PesquisarDataModal(this.Usuario.Name);
+
+            if (horariomodal == "False")
+            {
+                PesquisaModel.IncluirDataModal(usuario, dataModal);
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
---
+++ PesquisaSatisfacao/Models/PesquisaModel.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesquisaSatisfacao.Models
+{
+    public class PesquisaModel
+    {
+        public string Texto { get; set; }
+        public int Nota { get; set; }
+        public string Usuario { get; set; }
+        public DateTime DataRealizada { get; set; }
+
+
+        public static bool Incluir(string texto, int nota, string usuario, DateTime data)
+        {
+            var repositorio = Repositorio.NovaInstancia();
+
+            var sql = @"UPDATE pesquisa_satisfacao 
+                        set texto = :texto,
+                            nota = :nota,
+                            data = :data
+                        where usuario = :usuario";
+
+            var parametros = repositorio.NovosParametros();
+            parametros.Add(":texto", texto);
+            parametros.Add(":nota", nota);
+            parametros.Add(":usuario", usuario);
+            parametros.Add(":data", data);
+
+            var resultado = repositorio.ExecutarSql(sql, parametros);
+
+            return resultado;
+        }
+
+        public static bool EncontrarPesquisaDuplicada(string usuario)
+        {
+            var repositorio = Repositorio.NovaInstancia();
+            var parametros = repositorio.NovosParametros();
+
+            var sql = @"SELECT data from pesquisa_satisfacao where usuario = :usuario";
+
+            parametros.Add(":usuario", usuario);
+
+            var resultado = repositorio.ExecutarSqlComRetorno(sql, parametros);
+
+            if (resultado.Tables[0].Rows.Count > 0)
+            {
+                if (resultado.Tables[0].Rows[0].ItemArray[0].ToString() != "")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IncluirDataModal(string usuario, DateTime data)
+        {
+            var repositorio = Repositorio.NovaInstancia();
+
+            var sql = @"INSERT INTO pesquisa_satisfacao ( usuario, datamodal)
+                        VALUES (:usuario, :data)";
+
+            var parametros = repositorio.NovosParametros();
+
+            parametros.Add(":usuario", usuario);
+            parametros.Add(":data", data);
+
+            var resultado = repositorio.ExecutarSql(sql, parametros);
+
+            return resultado;
+        }
+
+        public static bool AtualizarDataModal(string usuario, DateTime data)
+        {
+            var repositorio = Repositorio.NovaInstancia();
+
+            var sql = @"UPDATE pesquisa_satisfacao set datamodal = :data where usuario = :usuario";
+
+            var parametros = repositorio.NovosParametros();
+
+            parametros.Add(":usuario", usuario);
+            parametros.Add(":data", data);
+
+            var resultado = repositorio.ExecutarSql(sql, parametros);
+
+            return resultado;
+        }
+
+        public static string PesquisarDataModal(string usuario)
+        {
+            var repositorio = Repositorio.NovaInstancia();
+            var parametros = repositorio.NovosParametros();
+
+            var sql = @"SELECT datamodal from pesquisa_satisfacao where usuario = :usuario";
+
+            parametros.Add(":usuario", usuario);
+            var resultado = repositorio.ExecutarSqlComRetorno(sql, parametros);
+
+
+            if (resultado.Tables[0].Rows.Count > 0)
+            {
+                return resultado.Tables[0].Rows[0].ItemArray[0].ToString();
+            }
+            return "False";
+        }
+    }
+}
